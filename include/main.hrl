10ead8fed1834e5d43179fc7c84558d06c211f55
joq62 <joq62@c50>
2024-04-03 11:18:22 +0200

    corr

--- include/main.hrl
+++ include/main.hrl
@@ -1,3 +1,3 @@
--define(ConnectNodes,['main_a@c50','main_a@c2000','main_a@c201','main_a@c202']).
+-define(ConnectNodes,['main_a@c50','main_a@c200','main_a@c201','main_a@c202']).
 -define(Sleep,30*1000).
 
